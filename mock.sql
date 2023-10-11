f89a86f6e01a72c8c5251ac165b0b3e24dc6320a
Wei <weihengjiang255@gmail.com>
2023-10-11 13:18:44 -0400

    add mock data

---
+++ mock.sql
@@ -0,0 +1,9 @@
+insert into `users` (`first_name`, `last_name`, `password_hash`, `credit_score`)
+values
+    ("Wei Heng", "Jiang", "hashed_password", 800),
+    ("Bob", "Briand", "hashed_password", 800),
+    ("Bob", "Broke", "hashed_password", 100),
+    ("Alice", "Amber", "hashed_password", 800),
+    ("Alice", "Ai", "hashed_password", 900),
+    ("Carl", "Crisp", "hashed_password", 800),
+    ("Diego", "Doom", "hashed_password", 400);
